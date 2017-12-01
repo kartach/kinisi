b1ce8a8768c1a22c1fdb73c002e01e34b3e582c3
d <d@localhost.localdomain>
2017-12-01 12:32:48 +0100

    SW Facebook fix (loads later using https)

--- modules/mod_sw_facebookdisplay/tmpl/default.php
+++ modules/mod_sw_facebookdisplay/tmpl/default.php
@@ -21,9 +21,19 @@ $show_faces = $params->get('show_faces');
 $header = $params->get('header');
 $border = $params->get('border');
 $print_facebook = '';
-$print_facebook .= '<iframe src="http://www.facebook.com/plugins/likebox.php?href='.$pageURL.'&amp;width='.$width.'&amp;colorscheme='.$color_scheme.'&amp;show_faces='.$show_faces.'&amp;connections='.$connections.'&amp;stream='.$streams.'&amp;show_border='.$border.'&amp;header='.$header.'&amp;height='.$height.'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px; height: '.$height.'px; " allowTransparency="true"></iframe>';
+$print_facebook .= '<iframe id="sw_facebook_iframe" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'.$width.'px; height: '.$height.'px; " allowTransparency="true"></iframe>';
 $print_facebook .= '';
+//$iframe_url = 'http://www.facebook.com/plugins/likebox.php?href='.$pageURL.'&amp;width='.$width.'&amp;colorscheme='.$color_scheme.'&amp;show_faces='.$show_faces.'&amp;connections='.$connections.'&amp;stream='.$streams.'&amp;show_border='.$border.'&amp;header='.$header.'&amp;height='.$height.'';
+$iframe_url = 'https://www.facebook.com/plugins/likebox.php?href='.$pageURL.'&width='.$width.'&colorscheme='.$color_scheme.'&show_faces='.$show_faces.'&connections='.$connections.'&stream='.$streams.'&show_border='.$border.'&header='.$header.'&height='.$height.'';
+
 ?>
+
+<script type="text/javascript">
+jQuery(document).ready(function($){
+    $('iframe#sw_facebook_iframe').attr('src', '<?php echo $iframe_url; ?>');    
+});
+</script>
+
 <div id="sw_facebook_display" class="<?php echo $params->get('moduleclass_sfx');?>">
 	<?php echo $print_facebook; ?>
 </div>
